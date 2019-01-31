45890581de37f76ac0762299751ce8267827b314
davazamb <david.zambrano10@gmail.com>
2019-01-31 12:09:49 -0300

    Metodo de exportacion se toma la data importada en sql y se despliega en una grilla.

--- ImportaExcelSql/Controllers/HomeController.cs
+++ ImportaExcelSql/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImportaExcelSql.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
@@ -21,87 +22,114 @@ namespace ImportaExcelSql.Controllers
         public ActionResult Index(HttpPostedFileBase file)
         {
             string filePath = string.Empty;
-            if (file != null)
+            try
             {
-                string path = Server.MapPath("~/Uploads/");
-                if (!Directory.Exists(path))
+                if (file != null)
                 {
-                    Directory.CreateDirectory(path);
-                }
-
-                filePath = path + Path.GetFileName(file.FileName);
-                string extension = Path.GetExtension(file.FileName);
-                file.SaveAs(filePath);
-
-                string conString = string.Empty;
-
-                switch (extension)
-                {
-                    case ".xls": //Excel 97-03.
-                        conString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HDR=YES'";
-                        break;
-                    case ".xlsx": //Excel 07 and above.
-                        conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HDR=YES'";
-                        break;
-                }
-
-                DataTable dt = new DataTable();
-                conString = string.Format(conString, filePath);
-
-                using (OleDbConnection connExcel = new OleDbConnection(conString))
-                {
-                    using (OleDbCommand cmdExcel = new OleDbCommand())
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
                     {
-                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                        Directory.CreateDirectory(path);
+                    }
+
+                    filePath = path + Path.GetFileName(file.FileName);
+                    string extension = Path.GetExtension(file.FileName);
+                    file.SaveAs(filePath);
+
+                    string conString = string.Empty;
+
+                    switch (extension)
+                    {
+                        case ".xls": //Excel 97-03.
+                            conString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HDR=YES'";
+                            break;
+                        case ".xlsx": //Excel 07 and above.
+                            conString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=YES'";
+                            break;
+                    }
+
+                    DataTable dt = new DataTable();
+                    conString = string.Format(conString, filePath);
+
+                    using (OleDbConnection connExcel = new OleDbConnection(conString))
+                    {
+                        using (OleDbCommand cmdExcel = new OleDbCommand())
                         {
-                            cmdExcel.Connection = connExcel;
+                            using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                            {
+                                cmdExcel.Connection = connExcel;
 
-                            //Get the name of First Sheet.
-                            connExcel.Open();
-                            DataTable dtExcelSchema;
-                            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                            string sheetName = dtExcelSchema.Rows[0]["TB_IMP_IMPORTADATOS"].ToString();
-                            connExcel.Close();
+                                //Get the name of First Sheet.
+                                connExcel.Open();
+                                DataTable dtExcelSchema;
+                                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                                string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                connExcel.Close();
 
-                            //Read Data from First Sheet.
-                            connExcel.Open();
-                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
-                            odaExcel.SelectCommand = cmdExcel;
-                            odaExcel.Fill(dt);
-                            connExcel.Close();
+                                //Read Data from First Sheet.
+                                connExcel.Open();
+                                cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                odaExcel.SelectCommand = cmdExcel;
+                                odaExcel.Fill(dt);
+                                connExcel.Close();
+                            }
+                        }
+                    }
+
+                    conString = @"Server=NBDZAMBRANOB\SQLEXPRESS;Database=BaseDZ;Trusted_Connection=True;";
+                    using (SqlConnection con = new SqlConnection(conString))
+                    {
+                        using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                        {
+                            //Set the database table name.
+                            sqlBulkCopy.DestinationTableName = "dbo.TB_IMP_IMPORTADATOS";
+
+                            // Map the Excel columns with that of the database table, this is optional but good if you do
+                            // 
+                            sqlBulkCopy.ColumnMappings.Add("Id", "PK_IMP_IMPORTADATOS_ID");
+                            sqlBulkCopy.ColumnMappings.Add("RutFacilitador", "TB_IMP_IMPORTADATOS_RutFacilitador");
+                            sqlBulkCopy.ColumnMappings.Add("NombreFacilitador", "TB_IMP_IMPORTADATOS_NombreFacilitador");
+                            sqlBulkCopy.ColumnMappings.Add("FechaIngreso", "TB_IMP_IMPORTADATOS_FechaIngreso");
+                            sqlBulkCopy.ColumnMappings.Add("FechaAsignación", "TB_IMP_IMPORTADATOS_FechaAsignación");
+                            sqlBulkCopy.ColumnMappings.Add("FechaEvaluación", "TB_IMP_IMPORTADATOS_FechaEvaluación");
+                            sqlBulkCopy.ColumnMappings.Add("NombreEvaluador", "TB_IMP_IMPORTADATOS_NombreEvaluador");
+                            sqlBulkCopy.ColumnMappings.Add("SectorModulo", "TB_IMP_IMPORTADATOS_SectorModulo");
+                            sqlBulkCopy.ColumnMappings.Add("SubSectorModulo", "TB_IMP_IMPORTADATOS_SubSectorModulo");
+                            sqlBulkCopy.ColumnMappings.Add("TipoModulo", "TB_IMP_IMPORTADATOS_TipoModulo");
+                            sqlBulkCopy.ColumnMappings.Add("PlanFormativo", "TB_IMP_IMPORTADATOS_PlanFormativo");
+                            sqlBulkCopy.ColumnMappings.Add("NombreModulo", "TB_IMP_IMPORTADATOS_NombreModulo");
+                            sqlBulkCopy.ColumnMappings.Add("Estado", "TB_IMP_IMPORTADATOS_Estado");
+                            sqlBulkCopy.ColumnMappings.Add("Correo", "TB_IMP_IMPORTADATOS_Correo");
+                            sqlBulkCopy.ColumnMappings.Add("Teléfono", "TB_IMP_IMPORTADATOS_Teléfono");
+                            sqlBulkCopy.ColumnMappings.Add("Comuna", "TB_IMP_IMPORTADATOS_Comuna");
+                            sqlBulkCopy.ColumnMappings.Add("Región", "TB_IMP_IMPORTADATOS_Región");
+                            sqlBulkCopy.ColumnMappings.Add("FechaEnvio", "TB_IMP_IMPORTADATOS_FechaEnvio");
+
+                            con.Open();
+                            sqlBulkCopy.WriteToServer(dt);
+                            con.Close();
                         }
                     }
                 }
+                //if the code reach here means everthing goes fine and excel data is imported into database
+                ViewBag.Success = "Archivo excel importado y guardado en la base de datos";
 
-                conString = @"Server=NBDZAMBRANOB\SQLEXPRESS;Database=BaseDZ;Trusted_Connection=True;";
-                using (SqlConnection con = new SqlConnection(conString))
-                {
-                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
-                    {
-                        //Set the database table name.
-                        sqlBulkCopy.DestinationTableName = "dbo.TB_IMP_IMPORTADATOS";
-
-                        // Map the Excel columns with that of the database table, this is optional but good if you do
-                        // 
-                        sqlBulkCopy.ColumnMappings.Add("Id", "Id");
-
-                        con.Open();
-                        sqlBulkCopy.WriteToServer(dt);
-                        con.Close();
-                    }
-                }
             }
-            //if the code reach here means everthing goes fine and excel data is imported into database
-            ViewBag.Success = "Archivo excel importado y guardado en la base de datos";
-
+            catch (Exception ex)
+            {               
+                ViewBag.Success = "Se genero un error" + ex.Message;
+            }
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            List<TB_IMP_IMPORTADATOS> datos = new List<TB_IMP_IMPORTADATOS>();
+            using (var context = new BaseDZEntities())
+            {
+                datos = context.TB_IMP_IMPORTADATOS.ToList();
+            }
+            return View(datos);
         }
 
         public ActionResult Contact()
